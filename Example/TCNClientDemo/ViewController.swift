0d7ad0e694f9b1c7d3b867befe61d7a726ca6d4b
K. Shankari <shankari@eecs.berkeley.edu>
2020-04-14 22:19:07 -0700

    Enable cocoapod creation for this repo
    The real changes are in `TCNClient.podspec`.
    The standard command to create a pod also allows you to create an example of
    using the pod.
    
    I am not sure this is useful for this library, but it is easier to delete the example than to reconstruct it using boilerplate later.
    
    Testing done:
    
    pod itself compiles
    
    ```
    tcn-client-ios shankari$ pod lib lint TCNClient.podspec --allow-warnings
    
     -> TCNClient (0.1.0)
        - NOTE  | xcodebuild:  note: Using new build system
        - NOTE  | xcodebuild:  note: Building targets in parallel
        - NOTE  | [iOS] xcodebuild:  note: Planning build
        - NOTE  | [iOS] xcodebuild:  note: Constructing build description
        - NOTE  | [iOS] xcodebuild:  warning: Skipping code signing because the target does not have an Info.plist file and one is not being generated automatically. (in target 'App' from project 'App')
        - WARN  | [iOS] xcodebuild:  /Users/shankari/OSS/tcn-client-ios/Sources/TCNClient/Crypto/DataRepresentable.swift:34:29: warning: initialization of 'UnsafeBufferPointer<Self>' results in a dangling buffer pointer
        - NOTE  | [iOS] xcodebuild:  /Users/shankari/OSS/tcn-client-ios/Sources/TCNClient/Crypto/DataRepresentable.swift:34:56: note: implicit argument conversion from 'Self' to 'UnsafePointer<Self>?' produces a pointer valid only for the duration of the call to 'init(start:count:)'
        - NOTE  | [iOS] xcodebuild:  /Users/shankari/OSS/tcn-client-ios/Sources/TCNClient/Crypto/DataRepresentable.swift:34:56: note: use 'withUnsafePointer' in order to explicitly convert argument to pointer valid for a defined scope
        - NOTE  | [iOS] xcodebuild:  note: Execution policy exception registration failed and was skipped: Error Domain=NSPOSIXErrorDomain Code=1 "Operation not permitted" (in target 'TCNClient' from project 'Pods')
        - NOTE  | [iOS] xcodebuild:  note: Execution policy exception registration failed and was skipped: Error Domain=NSPOSIXErrorDomain Code=1 "Operation not permitted" (in target 'Pods-App' from project 'Pods')
        - NOTE  | [iOS] xcodebuild:  note: Execution policy exception registration failed and was skipped: Error Domain=NSPOSIXErrorDomain Code=1 "Operation not permitted" (in target 'App' from project 'App')
    
    TCNClient passed validation.
    ```
    
    demo project compiles (see screenshot in PR)

---
+++ Example/TCNClientDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TCNClientDemo
+//
+//  Created by shankari on 04/14/2020.
+//  Copyright (c) 2020 shankari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
